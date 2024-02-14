d7d7b6d84eaa7aa2b98bf55d81848461685d491c
me.wash <64738032+me-wash@users.noreply.github.com>
2024-02-14 13:23:26 +0100

    g

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.body;
+  const lamp = document.getElementById("mode");
+
+  const toggleTheme = () => {
+      const currentTheme = body.getAttribute("data-theme");
+      if (currentTheme === "dark") {
+          body.setAttribute("data-theme", "light");
+          localStorage.setItem("theme", "light");
+      } else {
+          body.setAttribute("data-theme", "dark");
+          localStorage.setItem("theme", "dark");
+      }
+  };
+
+  lamp.addEventListener("click", toggleTheme);
+  initTheme(localStorage.getItem("theme"));
+  setTimeout(() => body.classList.remove("notransition"), 75);
+});
+
+const initTheme = (state) => {
+  const body = document.body;
+  if (state === "dark") {
+      body.setAttribute("data-theme", "dark");
+  } else {
+      body.removeAttribute("data-theme");
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Initial setup: show the Board group by default
+  showGroup('board');
+
+  // Handle tab button click
+  var tabBtns = document.querySelectorAll('.tab-btn');
+  tabBtns.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+      var group = this.getAttribute('data-group');
+      showGroup(group);
+      tabBtns.forEach(function(otherBtn) {
+        if (otherBtn !== btn) {
+          otherBtn.classList.remove('active');
+        }
+      });
+      btn.classList.toggle('active');
+    });
+  });
+
+  function showGroup(group) {
+    // Hide all columns
+    var columns = document.querySelectorAll('.column');
+    columns.forEach(function(column) {
+      column.classList.remove('active');
+    });
+
+    // Show only columns with the selected group
+    var selectedColumns = document.querySelectorAll(`.column[data-group="${group}"]`);
+    selectedColumns.forEach(function(selectedColumn) {
+      selectedColumn.classList.add('active');
+    });
+  }
+});
+(() => {
+  // Blur the content when the menu is open
+  const cbox = document.getElementById("menu-trigger");
+
+  cbox.addEventListener("change", function () {
+    const area = document.querySelector(".wrapper");
+    this.checked
+      ? area.classList.add("blurry")
+      : area.classList.remove("blurry");
+  });
+})();
+
+
